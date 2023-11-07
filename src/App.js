c7f77515a2151dce45b7c08ad6168189f7aa13a7
swatiagr22 <swati9926agrawal@gmail.com>
2023-11-07 13:40:22 +0530

    to-do-complete-code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 const App = () =>
 {
   const [todo, setTodo] = useState([])
-  const [counter, setCounter]=useState(1)
+  const [editingFlag, setEditing]=useState(-1)
   
   function addTodo()
   {
@@ -11,14 +11,26 @@ const App = () =>
      let tempTodo = document.getElementById("todoInput").value 
     console.log("tempTodo : "+tempTodo);
 
-    addToArray(tempTodo, true)
-    document.getElementById("todoInput").value = ""
+    if (todo.length>0)
+    {
+      addToArray(todo[todo.length-1].id+1,tempTodo,false)
+      
+    }
+    else
+    {
+      addToArray(0,tempTodo,false)
+      document.getElementById("todoInput").value=""
+
+    }
+
+    // addToArray(tempTodo, true)
+    // document.getElementById("todoInput").value = ""
   }
     
-    function addToArray(text, completed) {
+    function addToArray(id,text, completed) {
       
       let tempTodoObject = {
-        id: counter,
+        id: id,
         text: text,
         completed: completed
         
@@ -26,18 +38,22 @@ const App = () =>
       todo.push(tempTodoObject)
       console.log(todo)
       setTodo([...todo])
-      setCounter(counter+1)
-    
     }
     
   function deleteTodo(id)
    {
        console.log("------------------------------DeleteTodo-------------------------------");
-      let tempTodo= todo.filter(element=>{
+       console.log("id:"+id);
+       console.log(todo);
+      let tempTodo= todo.filter(element=>
+        {
         return element.id !==id
-       })
-    setTodo([...tempTodo])
+        })
+
+        console.log(tempTodo);
+        setTodo([...tempTodo])
   }
+
   function mock()
   {
     if(todo.length>0)
@@ -50,13 +66,51 @@ const App = () =>
 
     else
     {
-    addToArray(todo[todo.length-1].id+1,"Todo 1", true)
+    addToArray(0,"Todo 1", true)
     addToArray(todo[todo.length-1].id+1,"Todo 2", false)
     addToArray(todo[todo.length-1].id+1,"Todo 3", false)
     addToArray(todo[todo.length-1].id+1,"Todo 4", false)
     }
 
   }
+
+  function checkListener(id)
+  {
+    console.log("checkListener");
+    console.log(todo);
+    todo.map(element=>
+      {
+        if (element.id == id) 
+        {
+          element.completed = !element.completed
+        }
+        return element
+      })
+      console.log(todo);
+      setTodo([...todo])
+
+  }
+
+  function editTodo(id)
+  {
+    console.log("editTodo");
+    setEditing(id)
+  }
+   function updateTodo ()
+   {
+      console.log("updateTodo");
+      console.log("editingFlag : " +editingFlag);
+      let tempTodo = todo.map(element=>
+        {
+          if (element.id == editingFlag) 
+          {
+             element.text = document.getElementById("editTodo").value  
+          }
+          return element
+        })
+        setEditing (-1)
+        setTodo([...tempTodo])
+   }
   return <div>
       <h1>To-do Application</h1>
       <button onClick={()=>mock()}>mock</button>
@@ -69,28 +123,35 @@ const App = () =>
             
             {
             element.completed ? 
+            //compeleted todo
             <div>
-              <input type="checkbox" checked/>
+              <input type="checkbox" onClick={()=>checkListener(element.id)} checked/>
               <s>{element.text+" "}</s>
               <button onClick={()=>deleteTodo(element.id)}>Delete</button>
             </div> : 
-
+            //incompelete todo
+            (element.id == editingFlag?
+            //editing frontend
             <div>
-
-            <input type="checkbox"></input>
-            {element.text+"  "} 
-            <button onClick={()=>deleteTodo(element.id)} >Delete</button>
-
-            </div>
-        
-            } 
-            </div>
-          }) 
+              <input type="checkbox" onClick={()=>checkListener(element.id)}></input>
+              <input type="text" Value={element.text} placeholder="Update Todo here" id="editTodo"/>
+              <button onClick={()=>deleteTodo(element.id)}>DELETE</button>
+              <button onClick={()=>updateTodo()}>Save Todo</button>
+              </div> :
+             <div>
+             <input type="checkbox" onClick={()=>checkListener(element.id)} ></input>
+             {element.text+"   "}
+             <button onClick={()=>deleteTodo(element.id)}>Delete</button>
+             <button onClick={()=>editTodo(element.id)}>Edit</button>
+           </div>  
+         )
       }
       
       </div>
         
-      
+        })
+      }
+      </div>  
 
 
   }
